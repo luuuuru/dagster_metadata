88b8cdd6c98dc11d7a8f65aa60a25cd9a65bae03
luuuuru <127566965+luuuuru@users.noreply.github.com>
2026-04-29 12:21:16 +0200

    Create column_schema.py

---
+++ column_schema.py
@@ -0,0 +1,33 @@
+column_schema = dg.TableSchema( 
+   columns=[ 
+       dg.TableColumn( 
+           name="",  
+           type="", # "string", "int", "float", "bool" 
+           description="", 
+           tags={ 
+               "required": "False", 
+               # --- Provenance / Data Dictionary --- 
+               "data_dict_format": "N/A", 
+               "data_dict_unit": "N/A", 
+               "data_dict_valid_values": "N/A", 
+               "data_dict_cohort_filter": "N/A", 
+               "semantic_interoperability": "N/A" 
+           } 
+       ), 
+       dg.TableColumn( 
+           name="",  
+           type="",  
+           description="", 
+           tags={ 
+               "required": "False", 
+               "data_dict_format": "N/A", 
+               "data_dict_unit": "N/A", 
+               "data_dict_valid_values": "N/A", 
+               "data_dict_cohort_filter": "N/A", 
+               "semantic_interoperability": "N/A" 
+           } 
+       ), 
+   ] 
+) 
+
+ 
